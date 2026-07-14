2847486686e22879b7bb59b9c2123c4d454b1120
pongsatornbunnapho <pongsatornbunnapho@gmail.com>
2026-07-14 17:27:22 +0700

    2513310165Submit LabSheet03

---
+++ LabSheet03/src/NumberList.java
@@ -0,0 +1,39 @@
+
+public class NumberList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList numlist = new LinkedList();
+		
+		numlist.insert(0, " 37 ");
+		numlist.insert(0, " 7 ");
+		numlist.insert(0, " 4 ");
+		numlist.insert(0, " 16 ");
+		System.out.println(numlist.traversal());
+		
+		numlist.insert(2, " 20 ");
+		System.out.println(numlist.traversal());
+		
+		numlist.insert(25);
+		System.out.println(numlist.traversal());
+		
+		numlist.remove(0);
+		System.out.println(numlist.traversal());
+		
+		numlist.remove(2);
+		System.out.println(numlist.traversal());
+		
+		numlist.removeLastElement();
+		System.out.println(numlist.traversal());
+		
+		System.out.println("Length = " + numlist.length());
+		System.out.println("Node 2 = " + numlist.get(2));
+		
+		numlist.set(1, 100);
+		System.out.println(numlist.traversal());
+		
+		numlist.clear();
+		System.out.println(numlist.traversal());
+	}
+
+}
