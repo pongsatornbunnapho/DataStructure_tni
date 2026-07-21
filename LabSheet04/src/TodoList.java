37374c11a21c2001f22308ed46656207bce8cd50
pongsatornbunnapho <pongsatornbunnapho@gmail.com>
2026-07-21 16:59:14 +0700

    2513310165 Submit LabSheet04

---
+++ LabSheet04/src/TodoList.java
@@ -0,0 +1,25 @@
+
+public class TodoList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        DoublyLinkedList todoList = new DoublyLinkedList();
+        
+        todoList.insert("Finish Homework");
+        todoList.insert("Laundry");
+        todoList.insert("Grop Meeting");
+        System.out.println("To-Do List = " + todoList.traversal());
+        todoList.insert(0, "Submit Report");
+        System.out.println("To-Do List = " + todoList.traversal());
+        todoList.insert(2,"Buy Food");
+        System.out.println("To-Do List = " + todoList.traversal());
+        todoList.insert("Go to Gym");
+        System.out.println("To-Do List = " + todoList.traversal());
+        todoList.remove();
+        System.out.println("To-Do List = " + todoList.traversal());
+        todoList.remove(3);
+        System.out.println("To-Do List = " + todoList.traversal());
+	}
+
+}
